c2d7b1b291bd31b19ce684bcbf5ebc7ba329d97b
Nicolas <niikolas3131@gmail.com>
2022-06-17 09:56:42 -0500

    Prueba 2 jenkins

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/persona', personaRoute);
 
 app.get('/', (req, res) => {
-  res.send("PRUEBAS DE JENKINS");
+  res.send("ESTA ES UNA PRUEBA DE JENKINS");
 });
 
 app.listen(port, () => {
